034d7aee5113b1fe27fdf9ee44327e23cfe06c3b
danielevitale <danvitale@outlook.it>
2019-05-15 01:45:45 +0200

    Fine Milestone 1

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+// Quando si passa con il mouse sulle conversazioni cambia il loro colore di background
 $(".messages").mouseenter(function() {
   $(this).css("background", "#e5e4e3");
 })
 $(".messages").mouseleave(function() {
   $(this).css("background", "white");
+});
+
+// Al click sul tasto di invio messaggio, il messaggio compare sullo schermo
+$(".fas.fa-play").click(function() {
+  if($(".invio_mess input").val() != '') {
+    var template = $(".contenitore_messaggio.nascosto").clone();
+    template.removeClass("nascosto");
+    var testo = $(".invio_mess input").val();
+    $(".invio_mess input").val('')
+    template.children(".messaggio_utente").text(testo)
+    $(".box-messages").append(template);
+  }
 })
