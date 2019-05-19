c226bd2ba2d095b9f5de9dfd37a1ec050f787c73
danielevitale <danvitale@outlook.it>
2019-05-19 23:22:09 +0200

    Fine esercizio

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 // Funzione che consente di mettere lo scroll della chat in basso consentendo di visualizzare i messaggi appena
 // vengono inviati
 function scrolldown() {
-  var scoll = $(".box-messages")[0];
-  scoll.scrollTop += 9999;
+var h_boxmessages = $(".box-messages")[0].scrollHeight;
+$(".box-messages").scrollTop(h_boxmessages);
 }
 
 // Funzione di invio messaggi dell'utente e risposta dell'template_interlocutore
@@ -22,8 +22,8 @@ function invio_risposta() {
     var time = new Date();
     // inserisco l'ora e i minuti attuali nello small del clone template_utente
     template_utente.find("small").text(time.getHours() + ':' + time.getMinutes());
-    // Inserisco il clone template_utente nel box-messages
-    $(".box-messages").append(template_utente);
+    // Inserisco il clone template_utente nel box-messages con classe active
+    $(".box-messages.active").append(template_utente);
     // Resetto l'input text
     $(".invio_mess input").val('');
     scrolldown(); // Per i messaggi dell'utente
@@ -39,8 +39,8 @@ function invio_risposta() {
       var ora = new Date();
       // inserisco l'ora e i minuti attuali nello small del clone template_interlocutore
       template_interlocutore.find("small").text(ora.getHours() + ':' + ora.getMinutes());
-      // Inserisco il clone template_interlocutore nel box-messages
-      $(".box-messages").append(template_interlocutore);
+      // Inserisco il clone template_interlocutore nel box-messages con classe active
+      $(".box-messages.active").append(template_interlocutore);
       scrolldown(); // Per i messaggi dell'interlocutore
     }, 1000);
   } // Chiusura if
@@ -50,17 +50,26 @@ function invio_risposta() {
 
 
 
-
 // Richiamo la funzione scroll per visualizzare la conversazione a partire dagli ultimi messaggi
 scrolldown();
 
+
 // Quando si passa con il mouse sulle conversazioni cambia il loro colore di background
 $(".messages").mouseenter(function() {
-  $(this).css("background", "#e5e4e3");
+  var classi = $(this).attr("class");
+  // Verifico che non si tratta della conversazione attualmente visualizzata (che ha la classe bggrey)
+  if (classi != "messages bggrey") {
+    $(this).addClass("bglightgrey");
+  }
 })
 $(".messages").mouseleave(function() {
-  $(this).css("background", "white");
+  var classe = $(this).attr("class");
+  // Verifico che non si tratta della conversazione attualmente visualizzata (che ha la classe bggrey)
+  if (classe != "messages bggrey") {
+    $(this).removeClass("bglightgrey");
+  }
 });
+
 
 // Quando si digita un tasto nell'area di input compare l'icona del messaggio
 $(".invio_mess input").keypress(function() {
@@ -95,6 +104,7 @@ $(".invio_mess input").keypress(function(event) {
   }
 })
 
+
 // Ricerca delle conversazioni con gli utenti
 $("input.ricerca_nome").keyup(function(event) {
   // Nascondo tutte le conversazioni
@@ -109,4 +119,41 @@ $("input.ricerca_nome").keyup(function(event) {
       $(this).parent(".testo").parent(".testo_messaggi").parent(".messages").show();
     }
   })
+})
+
+
+// Al click su una conversazione: il suo background diventa grigio, cambia il nome
+// e la foto del contatto visualizzato, viene visualizzata la conversazione con quel contatto
+$(".messages").click(function() {
+  // Aggiungo il background grigio a quella conversazione e rimuovo gli altri background
+  $(".messages").removeClass("bggrey");
+  $(this).removeClass("bglightgrey");
+  $(this).addClass("bggrey");
+  // Sostituisco nome e foto del contatto visualizzato
+  var nome_da_cambiare = $(this).find("h3").text();
+  $(".contatto h3").text(nome_da_cambiare);
+  var img = $(this).find("img").attr("src");
+  $(".contatto img").attr("src", img);
+  // Mostro la conversazione con quel contatto nel box-messages
+  $(".box-messages").removeClass("active");
+  var conversazione = $(this).attr("data-contatto");
+  $('.box-messages[data-conversazione="' + conversazione + '"]').addClass("active");
+})
+
+
+// Al click sul chevron-down dei messaggi dell'utente compare il panel con le opzioni
+$(document).on("click", ".messaggio_utente i", function(){
+  $(this).siblings(".panel").toggleClass("active");
+})
+// Al click sul chevron-down dei messaggi dell'interlocutore compare il panel con le opzioni
+$(document).on("click", ".messaggio_interlocutore i", function(){
+  $(this).siblings(".panel").toggleClass("active");
+})
+// Al click su Cancella messaggio del panel opzioni il messaggio dell'utente viene rimosso
+$(document).on("click", ".panel h5:last-child", function(){
+  $(this).closest(".contenitore_messaggio_utente").remove();
+})
+// Al click su Cancella messaggio del panel opzioni il messaggio dell'interlocutore viene rimosso
+$(document).on("click", ".panel h5:last-child", function(){
+  $(this).closest(".contenitore_messaggio_interlocutore").remove();
 })
